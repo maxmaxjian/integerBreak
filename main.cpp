ccf275006776512a80ec82aa548f690319fdc878
maxmaxjian <jianw812@gmail.com>
2016-10-02 18:56:07 -0700

    first edit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+using std::vector;
+
+class solution {
+public:
+  int integerBreak(int n) {
+    
+  }
+
+private:
+  vector<int> maxProd(int n, int numOfInts) {
+    vector<int> result;
+    if (numOfInts == 1) {
+      result.push_back(n);
+    }
+    else {
+      vector<vector<int>> cands;
+       
+    }
+  }
+};
